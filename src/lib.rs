4f6fe7e0caad42a83c19e2ddea2de082f8ebfe59
koizr <koizumi.ryo.dev@gmail.com>
2021-08-07 14:02:02 +0900

    impl Display

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 pub struct SubModule(i32);
 
 impl SubModule {
     pub fn new(value: i32) -> Self {
         Self(value)
+    }
+}
+
+impl Display for SubModule {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SubModule({})", self.0)
     }
 }
 
